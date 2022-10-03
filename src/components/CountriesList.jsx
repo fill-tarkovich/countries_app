c7d057b25dc31358ef8a0b46f416a20a0797e257
Filipp Tarkovich <fill.tarkovich@gmail.com>
2022-10-03 09:48:09 +0300

    integrates openweather api

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -2,12 +2,14 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import CountryCard from "./CountryCard";
 
-import { initializeCountries } from "../features/countriesSlice";
+import { initializeCountries, search } from "../features/countriesSlice";
+import { Link } from "react-router-dom";
 
 const CountriesList = () => {
   const dispatch = useDispatch();
   const countries = useSelector((state) => state.countries.countries);
   const loading = useSelector((state) => state.countries.isLoading);
+  const searchInput = useSelector((state) => state.countries.search);
 
   useEffect(() => {
     dispatch(initializeCountries());
@@ -21,17 +23,22 @@ const CountriesList = () => {
     <div>
       <div className="cards_list">
         {countries.map((country) => (
-          <CountryCard
+          <Link
+            to={`/countries/${country.name.common}`}
+            state={{ country: country }}
             key={country.name.official}
-            flag={country.flag}
-            name={country.name.common}
-            official={country.name.official}
-            population={country.population}
-            languages={Object.values(country.languages || {}).join(", ")}
-            currencies={Object.values(country.currencies || {})
-              .map((currency) => currency.name)
-              .join(", ")}
-          />
+          >
+            <CountryCard
+              flag={country.flag}
+              name={country.name.common}
+              official={country.name.official}
+              population={country.population}
+              languages={Object.values(country.languages || {}).join(", ")}
+              currencies={Object.values(country.currencies || {})
+                .map((currency) => currency.name)
+                .join(", ")}
+            />
+          </Link>
         ))}
       </div>
     </div>
--- src/components/SingleCountry.jsx
+++ src/components/SingleCountry.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const SingleCountry = () => {
-  return <div>SingleCountry</div>;
+  const location = useLocation();
+  const country = location.state.country;
+  const [weather, setWeather] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  // const navigate = useNavigate();
+
+  const KEY = "2fa44403ccb9a8475bc6fccef5a5cf0b";
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&units=metric&appid=${KEY}`
+      )
+      .catch((error) => {
+        console.log(error);
+        setError(true);
+        setLoading(false);
+      })
+      .then((res) => {
+        setWeather(res.data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <h2>{country.name.common}</h2>
+      <h3>{country.capital}</h3>
+      {!error && (
+        <div>
+          <p>
+            Right now it is <strong>{parseInt(weather.main.temp)} </strong>
+            degrees in {country.capital} and {weather.weather[0].description}
+          </p>
+          <img
+            src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            alt={weather.weather[0].description}
+          />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default SingleCountry;
